fe99f2e161809fec05bb708e92a0f867b22059a2
georg-stone <george@thingbomb.com>
2025-03-06 19:31:17 -0500

    fix counter adding 2 points

--- apps/new-tab/src/App.tsx
+++ apps/new-tab/src/App.tsx
@@ -1236,16 +1236,30 @@ const App: Component = () => {
                   class="flex items-center gap-2 px-4 py-2 !text-white"
                 >
                   <button
-                    onmousedown={() => setCounter(Number(counter()) - 1)}
-                    onclick={() => setCounter(Number(counter()) - 1)}
+                    onmousedown={(e) => {
+                      setCounter(Number(counter()) - 1);
+                      e.preventDefault();
+                    }}
+                    onclick={(e) => {
+                      if (e.detail === 0) {
+                        setCounter(Number(counter()) - 1);
+                      }
+                    }}
                     class="text-sm font-semibold"
                   >
                     <Minus class="h-5 w-5" fill="currentColor" />
                   </button>
                   <p class="select-none text-sm font-semibold">{counter()}</p>
                   <button
-                    onmousedown={() => setCounter(Number(counter()) + 1)}
-                    onclick={() => setCounter(Number(counter()) + 1)}
+                    onmousedown={(e) => {
+                      setCounter(Number(counter()) + 1);
+                      e.preventDefault();
+                    }}
+                    onclick={(e) => {
+                      if (e.detail === 0) {
+                        setCounter(Number(counter()) + 1);
+                      }
+                    }}
                     class="text-sm font-semibold"
                   >
                     <Plus class="h-5 w-5" fill="currentColor" />
