6cf6523810cefd492579d1e3195f430f4c029c78
George <g3eorge@pm.me>
2025-08-21 20:29:19 -0400

    Update README.md (#142)

--- apps/noisefill/src/pages/Support.jsx
+++
@@ -1,8 +0,0 @@
-export default function Support() {
-  return (
-    <div className="px-6">
-      For support, please email{" "}
-      <a href="mailto:hello@thingbomb.com">hello@thingbomb.com</a>.
-    </div>
-  );
-}
